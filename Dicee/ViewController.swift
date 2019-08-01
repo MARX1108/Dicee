5427467ec6779c74f34eabac3929c1dddab40ab7
Marx.W <boyuan@vt.edu>
2019-08-01 18:56:39 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Marx Wang on 2019/8/1.
+//  Copyright © 2019 Marx Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
